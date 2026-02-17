5186505242b70f65a1d291708d28381a9166da09
Romaisae Kadour <romaisaekadour0606@outlook.com>
2026-02-17 22:57:34 +0100

    order pagina toegevoegd

--- account-bestellingen.php
+++ account-bestellingen.php
@@ -1,1 +1,145 @@
-account-bestellingen.php
+<?php
+
+require_once('files/header.php');
+?>
+<div class="page-title-overlap bg-dark pt-4">
+        <div class="container d-lg-flex justify-content-between py-2 py-lg-3">
+          <div class="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb breadcrumb-light flex-lg-nowrap justify-content-center justify-content-lg-start">
+                <li class="breadcrumb-item"><a class="text-nowrap" href="index-2.html"><i class="ci-home"></i>Home</a></li>
+                <li class="breadcrumb-item text-nowrap"><a href="#">Account</a>
+                </li>
+                <li class="breadcrumb-item text-nowrap active" aria-current="page">Bestelgeschiedenis</li>
+              </ol>
+            </nav>
+          </div>
+          <div class="order-lg-1 pe-lg-4 text-center text-lg-start">
+            <h1 class="h3 text-light mb-0">Mijn bestellingen</h1>
+          </div>
+        </div>
+      </div>
+      <div class="container pb-5 mb-2 mb-md-4">
+        <div class="row">
+          <!-- Sidebar-->
+          <aside class="col-lg-4 pt-4 pt-lg-0 pe-xl-5">
+            <div class="bg-white rounded-3 shadow-lg pt-1 mb-5 mb-lg-0">
+              <div class="d-md-flex justify-content-between align-items-center text-center text-md-start p-4">
+                <div class="d-md-flex align-items-center">
+                  <div class="img-thumbnail rounded-circle position-relative flex-shrink-0 mx-auto mb-2 mx-md-0 mb-md-0" style="width: 6.375rem;"><span class="badge bg-warning position-absolute end-0 mt-n2" data-bs-toggle="tooltip" title="Reward points">384</span><img class="rounded-circle" src="img\shop\account\avatar123.jpg" alt="Ena Gardner"></div>
+                  <div class="ps-md-3">
+                    <h3 class="fs-base mb-0">Ena Gardner</h3><span class="text-accent fs-sm">E.gardner@example.com</span>
+                  </div>
+                </div><a class="btn btn-primary d-lg-none mb-2 mt-3 mt-md-0" href="#account-menu" data-bs-toggle="collapse" aria-expanded="false"><i class="ci-menu me-2"></i>Account menu</a>
+              </div>
+              <div class="d-lg-block collapse" id="account-menu">
+                <div class="bg-secondary px-4 py-3">
+                  <h3 class="fs-sm mb-0 text-muted">Dashboard</h3>
+                </div>
+                <ul class="list-unstyled mb-0">
+                  <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3 active" href="account-orders.html"><i class="ci-bag opacity-60 me-2"></i>Bestellingen<span class="fs-sm text-muted ms-auto">1</span></a></li>
+                  <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="account-wishlist.html"><i class="ci-heart opacity-60 me-2"></i>Verlanglijst<span class="fs-sm text-muted ms-auto">3</span></a></li>
+                  <li class="mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="account-tickets.html"><i class="ci-help opacity-60 me-2"></i>Support tickets<span class="fs-sm text-muted ms-auto">1</span></a></li>
+                </ul>
+                <div class="bg-secondary px-4 py-3">
+                  <h3 class="fs-sm mb-0 text-muted">Account instellingen</h3>
+                </div>
+                <ul class="list-unstyled mb-0">
+                  <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="account-profile.html"><i class="ci-user opacity-60 me-2"></i>Profiel info</a></li>
+                  <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="account-address.html"><i class="ci-location opacity-60 me-2"></i>adres</a></li>
+                  <li class="mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="account-payment.html"><i class="ci-card opacity-60 me-2"></i>Betaal methode</a></li>
+                  <li class="d-lg-none border-top mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="account-signin.html"><i class="ci-sign-out opacity-60 me-2"></i>Log uit</a></li>
+                </ul>
+              </div>
+            </div>
+          </aside>
+          <!-- Content  -->
+          <section class="col-lg-8">
+            <!-- Toolbar-->
+            <div class="d-flex justify-content-between align-items-center pt-lg-2 pb-4 pb-lg-5 mb-lg-3">
+              <div class="d-flex align-items-center">
+                <label class="d-none d-lg-block fs-sm text-light text-nowrap opacity-75 me-2" for="order-sort">Sort orders:</label>
+                <label class="d-lg-none fs-sm text-nowrap opacity-75 me-2" for="order-sort">Sort orders:</label>
+                <select class="form-select" id="order-sort">
+                  <option>Allemaal</option>
+                  <option>Afgeleverd</option>
+                  <option>In uitvoering</option>
+                  <option>Vertraagd</option>
+                  <option>Geanuleerd</option>
+                </select>
+              </div><a class="btn btn-primary btn-sm d-none d-lg-inline-block" href="account-signin.html"><i class="ci-sign-out me-2"></i>uitloggen</a>
+            </div>
+            <!-- Orders list-->
+            <div class="table-responsive fs-md mb-4">
+              <table class="table table-hover mb-0">
+                <thead>
+                  <tr>
+                    <th>Bestelling #</th>
+                    <th>Aankoop datum</th>
+                    <th>Status</th>
+                    <th>Totaal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="py-3"><a class="nav-link-style fw-medium fs-sm" href="#order-details" data-bs-toggle="modal">34VB5540K83</a></td>
+                    <td class="py-3">15 Januari, 2026</td>
+                    <td class="py-3"><span class="badge bg-info m-0">In uitvoering</span></td>
+                    <td class="py-3">€16,99</td>
+                  </tr>
+                  <tr>
+                    <td class="py-3"><a class="nav-link-style fw-medium fs-sm" href="#order-details" data-bs-toggle="modal">78A643CD409</a></td>
+                    <td class="py-3">09 December, 2025</td>
+                    <td class="py-3"><span class="badge bg-danger m-0">Geanuleerd</span></td>
+                    <td class="py-3"><span>€51,55</span></td>
+                  </tr>
+                  <tr>
+                    <td class="py-3"><a class="nav-link-style fw-medium fs-sm" href="#order-details" data-bs-toggle="modal">112P45A90V2</a></td>
+                    <td class="py-3">3 Augustus, 2025</td>
+                    <td class="py-3"><span class="badge bg-warning m-0">Vetraagd</span></td>
+                    <td class="py-3">€205,56</td>
+                  </tr>
+                  <tr>
+                    <td class="py-3"><a class="nav-link-style fw-medium fs-sm" href="#order-details" data-bs-toggle="modal">28BA67U0981</a></td>
+                    <td class="py-3">13 April,2024</td>
+                    <td class="py-3"><span class="badge bg-success m-0">Afgeleverd</span></td>
+                    <td class="py-3">€198,35</td>
+                  </tr>
+                  <tr>
+                    <td class="py-3"><a class="nav-link-style fw-medium fs-sm" href="#order-details" data-bs-toggle="modal">502TR872W2</a></td>
+                    <td class="py-3">14 Februari, 2024</td>
+                    <td class="py-3"><span class="badge bg-success m-0">Afgeleverd</span></td>
+                    <td class="py-3">€67,67</td>
+                  </tr>
+                  <tr>
+                    <td class="py-3"><a class="nav-link-style fw-medium fs-sm" href="#order-details" data-bs-toggle="modal">47H76G09F33</a></td>
+                    <td class="py-3">4 Juli, 2023</td>
+                    <td class="py-3"><span class="badge bg-success m-0">Afgeleverd</span></td>
+                    <td class="py-3">€86,40</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- Pagination-->
+            <nav class="d-flex justify-content-between pt-2" aria-label="Page navigation">
+              <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#"><i class="ci-arrow-left me-2"></i>Prev</a></li>
+              </ul>
+              <ul class="pagination">
+                <li class="page-item d-sm-none"><span class="page-link page-link-static">1 / 5</span></li>
+                <li class="page-item active d-none d-sm-block" aria-current="page"><span class="page-link">1<span class="visually-hidden">(current)</span></span></li>
+                <li class="page-item d-none d-sm-block"><a class="page-link" href="#">2</a></li>
+                <li class="page-item d-none d-sm-block"><a class="page-link" href="#">3</a></li>
+                <li class="page-item d-none d-sm-block"><a class="page-link" href="#">4</a></li>
+                <li class="page-item d-none d-sm-block"><a class="page-link" href="#">5</a></li>
+              </ul>
+              <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#" aria-label="Next">Next<i class="ci-arrow-right ms-2"></i></a></li>
+              </ul>
+            </nav>
+          </section>
+        </div>
+      </div>
+      <?php
+      require_once('files/footer.php');
+?>
