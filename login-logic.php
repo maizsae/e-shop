a6a98add131b3cc1dcaae4ad5fa65fbfa96cf2d8
Romaisae Kadour <romaisaekadour0606@outlook.com>
2026-02-17 18:09:04 +0100

    Wachtwoord gehashed

--- login-logic.php
+++ login-logic.php
@@ -1,12 +1,33 @@
 <?php 
 require_once('files/functions.php');
+$first_name = trim($_POST['first_name']);
+$last_name = trim($_POST['last_name']);
 $email = trim($_POST['email']);
+$password = trim($_POST['password']);
+$password_1 = trim($_POST['password_1']);
+$phone_number = trim($_POST['phone_number']);
+
+
+if ($password != $password_1){
+    die("wachtwoord komt niet overeen");
+}
+
 $sql = "SELECT * FROM users WHERE email ='{$email}'";
 $res =$conn->query($sql);
 
 if($res->num_rows > 0){
     die("Een gebruiker met dezelfde email bestaat al.");
 }
+$password = password_hash($password,PASSWORD_DEFAULT);
+die($password);
+$sql = "INSERT INTO users (
+       first_name,
+       last_name,
+       phone_number,
+       password,
+       email,
+       user_type
+)";
 
 echo "<pre>";
-print_r($_POST);
+print_r($_POST);    
