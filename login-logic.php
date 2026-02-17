3ad6c2c9ccf79b713ce96f8a44835f51f41270e2
Romaisae Kadour <romaisaekadour0606@outlook.com>
2026-02-17 17:55:10 +0100

    Controle toegevoegd op bestaand e-mailadres bij registratie

--- login-logic.php
+++ login-logic.php
@@ -2,6 +2,11 @@
 require_once('files/functions.php');
 $email = trim($_POST['email']);
 $sql = "SELECT * FROM users WHERE email ='{$email}'";
-die ($sql);
+$res =$conn->query($sql);
+
+if($res->num_rows > 0){
+    die("Een gebruiker met dezelfde email bestaat al.");
+}
+
 echo "<pre>";
 print_r($_POST);
